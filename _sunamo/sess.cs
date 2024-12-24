450ad521fd5530634803a9e42ad8f473291c61c8
sunamo.cz <radek.jancik@sunamo.cz>
2024-12-24 20:52:38 +0100

    Removed unused element from csproj

--- _sunamo/sess.cs
+++ _sunamo/sess.cs
@@ -2,7 +2,7 @@ namespace SunamoHttp._sunamo;
 
 internal class sess
 {
-    internal static string i18n(string contentType011)
+    internal static string FromKey(string contentType011)
     {
         return contentType011;
     }
