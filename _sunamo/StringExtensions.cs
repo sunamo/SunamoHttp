d87c221645706f1482da297e492fca5328874d66
sunamo.cz <radek.jancik@sunamo.cz>
2025-07-19 15:26:49 +0200

    feat: Made internal in _sunamo

--- _sunamo/StringExtensions.cs
+++ _sunamo/StringExtensions.cs
@@ -2,7 +2,7 @@ namespace SunamoHttp._sunamo;
 
 internal static class StringExtensions
 {
-public static string FromSpace160To32(this string input)
+internal static string FromSpace160To32(this string input)
 {
 return Regex.Replace(input, @"\p{Z}", " ");
 }
